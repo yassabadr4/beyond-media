874b3ca3b9c03e06ebd239a7e13b36755fe089f2
yassa badr <75986635+yassabadr4@users.noreply.github.com>
2024-07-23 23:57:07 +0300

    finish impression

--- lib/main.dart
+++ lib/main.dart
@@ -23,10 +23,9 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home:   HomeScreen(),
+        home: HomeScreen(),
         debugShowCheckedModeBanner: false,
       ),
     );
   }
 }
-
